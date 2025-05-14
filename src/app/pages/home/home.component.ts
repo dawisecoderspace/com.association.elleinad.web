0a25510575d907781514b1aca5eacdcbee97367c
Dawise <dawise@MacBook-Pro-de-Dawise.local>
2025-05-14 02:35:12 +0200

    add footer component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NgFor],
+  imports: [NgFor, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true
