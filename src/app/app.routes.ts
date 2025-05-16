65df3cbb7aaad7b487d1b471b49157a0dae0e45d
Dawise <dawise@MacBook-Pro-de-Dawise.local>
2025-05-17 00:47:31 +0200

    create page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PresentationComponent } from './pages/presentation/presentation.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [{
     path: '',
@@ -16,4 +17,7 @@ export const routes: Routes = [{
 }, {
     path: 'contact',
     component: ContactComponent
+}, {
+    path: '**',
+    component: NotFoundComponent
 }];
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.component.css'
 })
 export class NotFoundComponent {
+  countdown = 30;
+  private intervalId: any;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.countdown--;
+      if (this.countdown === 0) {
+        clearInterval(this.intervalId);
+        this.router.navigate(['/home']);
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
