1018134d8928de3aca2afa80bb1fdeed786b5ab8
Dawise <dawise@MacBook-Pro-de-Dawise.local>
2025-05-17 17:50:35 +0200

    version developpement 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,34 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
+import { NgIf, NgStyle } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, NgIf, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   baseHref = '/com.association.elleinad.web/';
+
+  showBubble = false;
+  bubbleX = 0;
+  bubbleY = 0;
+
+  updateMousePosition(event: MouseEvent) {
+    const offset = 15;
+    this.bubbleX = event.clientX + offset;
+    this.bubbleY = event.clientY + offset;
+  }
   
+  menuOuvert = false;
+
+  toggleMenu() {
+    this.menuOuvert = !this.menuOuvert;
+  }
+
   scrollToTop(event: Event) {
     event.preventDefault()
     window.scrollTo({ top: 0, behavior: 'smooth' });
